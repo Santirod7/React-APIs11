3d3c29dc30f864dbc1f1f2ecdeacc38ffa33fd67
Santiago Rodriguez <santiara010@gmail.com>
2024-09-18 21:27:01 -0300

    Marque los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './bootstrap.min.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <main className='container'>
+    <h1 className='text-center text-primary bg-light mt-4 py-2 '>Noticias</h1>
+    </main>
   )
 }
 
